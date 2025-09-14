c7f6bc65f9f23fcbc9b9c4906c9526cacaff151f
shoblinsky <timosh0307@gmail.com>
2025-09-14 11:42:51 +0500

    feat: 5 tests passing

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -86,8 +86,10 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  let result = 1;
+  result = -b / a;
+  return result;
 }
 
 /**
