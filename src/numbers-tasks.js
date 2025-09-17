27b7262d448456904bfed353ea87d96fbfce3fd6
shoblinsky <timosh0307@gmail.com>
2025-09-18 00:06:43 +0500

    feat: 51 tests passing

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -314,8 +314,13 @@ function getSumToN(/* n */) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  let res = 0;
+  const numString = num.toString();
+  for (let i = 0; i < numString.length; i += 1) {
+    res += parseInt(numString[i], 10);
+  }
+  return res;
 }
 
 /**
