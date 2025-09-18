b408136947966c64f6628c2792ea3329a5e6ace0
shoblinsky <timosh0307@gmail.com>
2025-09-18 13:08:28 +0500

    feat: 56 tests passing

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -288,8 +288,16 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) return 0;
+  let first = 1;
+  let second = 1;
+  for (let i = 3; i <= index; i += 1) {
+    const third = first + second;
+    first = second;
+    second = third;
+  }
+  return second;
 }
 
 /**
