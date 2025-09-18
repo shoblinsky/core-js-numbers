b6eb360935d7d818ec2ec097fa42a1b778643313
shoblinsky <timosh0307@gmail.com>
2025-09-18 11:31:20 +0500

    feat: 53 tests passing

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -252,8 +252,12 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const number = Number(value);
+  if (Number.isNaN(number)) {
+    return def;
+  }
+  return number;
 }
 
 /**
